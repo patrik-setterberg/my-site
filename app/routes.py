56b8ddc8b3b8592fd4950988f495e2daa3966bb4
Patrik Setterberg <setterberg.patrik@gmail.com>
2018-06-04 16:41:19 +0200

    Add forms and misc

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,34 @@
 from app import app
+from app.forms import LoginForm
+from flask import flash, redirect, render_template
 
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "herro"
+
+    user = {'username': 'admin'}
+
+    posts = [
+        {
+            'author': {'username': 'Kvarjo'},
+            'body': 'Riktigt najs idag'
+        },
+        {
+            'author': {'username': 'Admin'},
+            'body': 'jag håller med @Kvarjo'
+        }
+
+    ]
+    return render_template('index.html', title='Home', user=user, posts=posts)
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash('Login requested for user {}'.format(
+            form.username.data))
+        return redirect('/index')
+
+    return render_template('login.html', title='Sign In', form=form)
